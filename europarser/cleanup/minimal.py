330bf5808f7945228fdd4dfda992b2cc74cf8fe8
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2022-07-01 14:42:50 +0200

    Update minimal.py

--- europarser/cleanup/minimal.py
+++ europarser/cleanup/minimal.py
@@ -1,4 +1,4 @@
 from europarser.models import Pivot
 
-def minimal(pivots : List[Pivot], taillemin : int) -> List[Pivot]:
+def minimal(pivots : List[Pivot], taillemin : int = 300) -> List[Pivot]:
     return [e for e in pivots if "This extract may not display a well formed paragraph" not in e["texte"] and len(e["texte"]) > taillemin] 
