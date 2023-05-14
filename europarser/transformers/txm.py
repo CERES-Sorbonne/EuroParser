87029a88ec31be73254feb4d06eeb78a4da79154
Marceau-h <106751184+Marceau-h@users.noreply.github.com>
2023-05-14 10:59:01 +0200

    Erreur dans les attributs xml pour la détection de la langue

--- europarser/transformers/txm.py
+++ europarser/transformers/txm.py
@@ -17,9 +17,9 @@ class TXMTransformer(Transformer):
             f.write("<corpus>")
 
             for pivot in pivot_list:
-                print(pivot)
+                # print(pivot)
                 parsed = escape(pivot.texte.strip())
-                line = f"""<article titre="{re.sub('"', "'", escape(pivot.titre))}" date="{escape(pivot.date)}" journal="{escape(pivot.journal)}" journal_clean="{escape(pivot.journal_clean)}" keywords="{escape(pivot.keywords)} langue={escape(pivot.langue)}">"""
+                line = f"""<article titre="{re.sub('"', "'", escape(pivot.titre))}" date="{escape(pivot.date)}" journal="{escape(pivot.journal)}" journal_clean="{escape(pivot.journal_clean)}" keywords="{escape(pivot.keywords)}" langue="{escape(pivot.langue)}">"""
                 f.write(line)
                 f.write(parsed)
                 f.write("</article>")
