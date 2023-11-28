28f1758b8077b425b10fe460205629fb0ce448e9
Marceau <git@marceau-h.fr>
2023-11-28 17:49:02 +0100

    no reload and prefixed with folder

--- start_europarser.sh
+++ start_europarser.sh
@@ -8,19 +8,20 @@ cd $FOLDER || exit
 
 git pull origin master --quiet || exit
 
-source venv/bin/activate || exit
+python3.11 -m venv venv
+source $FOLDER/venv/bin/activate || exit
 pip3 install -U pip --quiet || exit
-pip3 install -r requirements.txt --quiet || exit
-pip3 install -r requirements-api.txt --quiet || exit
+pip3 install -r $FOLDER/requirements.txt --quiet || exit
+pip3 install -r $FOLDER/requirements-api.txt --quiet || exit
 
-IS_RUNNING=$(ps -aux | grep gunicorn | grep europarser_api)
+IS_RUNNING=$(ps -aux | grep uvicorn | grep europarser_api)
 if [ -z "$IS_RUNNING" ]
 then
     echo "europarser service currently not running, starting gunicorn..."
-    screen -S EuropressParser -dm bash -c "source /home/marceau/GH/EuropressParser/venv/bin/activate; python -m uvicorn europarser_api.api:app --port $EUROPARSER_PORT --root-path /europarser --workers 8 --limit-max-requests 8 --reload-dir /home/marceau/GH/EuropressParser --timeout-keep-alive 1000 --log-config log.conf"
+    screen -S EuropressParser -dm bash -c "source /home/marceau/GH/EuropressParser/venv/bin/activate; python -m uvicorn europarser_api.api:app --port $EUROPARSER_PORT --root-path /europarser --workers 8 --limit-max-requests 8 --timeout-keep-alive 1000 --log-config log.conf"
 else
     echo "europarser already running, restarting..."
     screen -S EuropressParser -X quit
-    screen -S EuropressParser -dm bash -c "source /home/marceau/GH/EuropressParser/venv/bin/activate; python -m uvicorn europarser_api.api:app --port $EUROPARSER_PORT --root-path /europarser --workers 8 --limit-max-requests 8 --reload-dir /home/marceau/GH/EuropressParser --timeout-keep-alive 1000 --log-config log.conf"
+    screen -S EuropressParser -dm bash -c "source /home/marceau/GH/EuropressParser/venv/bin/activate; python -m uvicorn europarser_api.api:app --port $EUROPARSER_PORT --root-path /europarser --workers 8 --limit-max-requests 8 --timeout-keep-alive 1000 --log-config log.conf"
 fi
 cd -
