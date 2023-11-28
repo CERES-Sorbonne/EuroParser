c98b33948fa5d465c6cd4ba78c68d01ca3f447d9
Marceau <git@marceau-h.fr>
2023-11-28 16:44:08 +0100

    Iramuteq accents

--- europarser/transformers/iramuteq.py
+++ europarser/transformers/iramuteq.py
@@ -1,13 +1,19 @@
 import io
 import re
+import unicodedata
 from typing import List
 
 from europarser.models import Pivot
 from europarser.transformers.transformer import Transformer
 
 
+def strip_accents(s):
+    return ''.join(c for c in unicodedata.normalize('NFKD', s) if unicodedata.category(c) != 'Mn')
+
+
 class IramuteqTransformer(Transformer):
     banned_keys = {"texte", "complement", "date", "epoch"}
+
     def __init__(self):
         super(IramuteqTransformer, self).__init__()
 
@@ -15,18 +21,18 @@ class IramuteqTransformer(Transformer):
         with io.StringIO() as f:
             for pivot in pivot_list:
                 dic = pivot.dict(exclude=self.banned_keys)
-                f.write(f"""**** {' '.join([f"*{k}_{self._format_value(str(v))}" for k,v in dic.items()])}\n""")
+                f.write(f"""**** {' '.join([f"*{k}_{self._format_value(str(v))}" for k, v in dic.items()])}\n""")
                 f.write(pivot.texte)
                 f.write('\n\n')
             return f.getvalue()
 
     @staticmethod
     def _format_value(value: str):
-        value = re.sub(r"[éèê]", "e", value)
-        value = re.sub(r"ô", "o", value)
-        value = re.sub(r"à", "a", value)
-        value = re.sub(r"œ", "oe", value)
-        value = re.sub(r"[ïîì]", "i", value)
+        # value = re.sub(r"[éèê]", "e", value)
+        # value = re.sub(r"ô", "o", value)
+        # value = re.sub(r"à", "a", value)
+        # value = re.sub(r"œ", "oe", value)
+        # value = re.sub(r"[ïîì]", "i", value)
+        value = strip_accents(value)
         value = re.sub(r"""[-\[\]'":().=?!,;<>«»—^*\\/|]""", ' ', value)
         return ''.join([w.capitalize() for w in value.split(' ')])
-
