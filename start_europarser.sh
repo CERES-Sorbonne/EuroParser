dc882ff01c9a8d7b8037187ef97aea36ca4bb471
Marceau <git@marceau-h.fr>
2023-12-05 09:28:58 +0100

    Start script checks for $EUROPARSER_OUTPUT

--- start_europarser.sh
+++ start_europarser.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ "$EUROPARSER_OUTPUT" ]
+then
+    echo "EUROPARSER_OUTPUT is set to '$EUROPARSER_OUTPUT'"
+else
+    echo "EUROPARSER_OUTPUT is not set, if you continue, no output will be saved"
+    read -p "Continue? [y/N] " -n 1 -r < /dev/tty
+    echo
+    if [[ ! $REPLY =~ ^[Yy]$ ]]
+    then
+        echo "Aborting"
+        exit 1
+    else
+        echo "Caution, no output will be saved"
+    fi
+fi
+
+
 export EUROPARSER_SERVER=https://ceres.huma-num.fr/europarser
 export EUROPARSER_PORT=8001
 export FOLDER=/home/marceau/GH/EuropressParser
