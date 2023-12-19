e0cd588826f606b13d88d46353efdd0c96d9f7cc
Marceau-h <git@marceau-h.fr>
2023-12-19 22:14:01 +0100

    Multiple and simple examples to use it locally

--- examples/multiple.py
+++ examples/multiple.py
@@ -1,12 +1,37 @@
-import os.path
+from pathlib import Path
 
-from europarser import FileToTransform, pipeline
+from europarser import pipeline
+from europarser.models import FileToTransform, TransformerOutput, Params, Output
 
-if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), 'resources', '1.HTML'), 'r') as f:
-        fake_file = FileToTransform(file=f.read(), name="fake_file_1")
 
-    with open(os.path.join(os.path.dirname(__file__), 'resources', '1.HTML'), 'r') as f:
-        fake_file_2 = FileToTransform(file=f.read(), name="fake_file_2")
+def main(folder: Path, outputs: list[Output]) -> None:
+    # parse all files
+    files = []
+    for file in folder.iterdir():
+        if file.is_file():
+            with open(file, 'r', encoding='utf-8') as f:
+                files.append(FileToTransform(name=file.name, file=f.read()))
+    print(f"Processing {len(files)} files...")
 
-    xml_res = pipeline([fake_file, fake_file_2], outputs="txm")
+    # process result
+    results: list[TransformerOutput] = pipeline(files, outputs, Params())
+
+    # write results
+    folder = folder / 'results'
+    folder.mkdir(exist_ok=True)
+    for result in results:
+        print(result.filename)
+        if isinstance(result.data, str):
+            with open(folder / f'{result.filename}', 'w', encoding='utf-8') as f:
+                    f.write(result.data)
+        else:
+            with open(folder / f'{result.filename}', 'wb') as f:
+                f.write(result.data)
+
+
+
+if __name__ == '__main__':
+    folder = Path('/home/marceau/Nextcloud/eurocollectes/15-18')
+    # outputs = ["json", "txm", "iramuteq", "csv", "stats", "processed_stats", "plots", "markdown"]
+    outputs = ["json", "stats", "processed_stats", "plots"]
+    main(folder, outputs)
--- examples/simple.py
+++ examples/simple.py
@@ -1,9 +1,28 @@
-import os.path
+from pathlib import Path
 
 from europarser import process
-import json
+from europarser.models import FileToTransform, TransformerOutput, Params, Output
 
-if __name__ == "__main__":
-    with open(os.path.join(os.path.dirname(__file__), 'resources', '1.HTML'), 'r') as f:
-        xml_res = process(f.read(), output="csv")
-        json_res = json.loads(process(f.read(), output="json"))
+
+def main(file: Path, outputs: list[Output]) -> None:
+    with open(file, 'r', encoding='utf-8') as f:
+        file = FileToTransform(name=file.name, file=f.read())
+
+    results: list[TransformerOutput] = process(file, outputs, Params())
+
+    folder = file.parent / 'results'
+    folder.mkdir(exist_ok=True)
+    for result in results:
+        print(result.filename)
+        if isinstance(result.data, str):
+            with open(folder / f'{result.filename}', 'w', encoding='utf-8') as f:
+                f.write(result.data)
+        else:
+            with open(folder / f'{result.filename}', 'wb') as f:
+                f.write(result.data)
+
+
+if __name__ == '__main__':
+    file = Path(__file__).parent / 'resources' / '1.HTML'
+    outputs = ["json", "stats", "processed_stats", "plots"]
+    main(file, outputs)
