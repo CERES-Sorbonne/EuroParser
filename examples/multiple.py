765c8802d9782334724a103774ad0e0f1557657f
Marceau <git@marceau-h.fr>
2023-12-20 18:00:59 +0100

    Params in the example

--- examples/multiple.py
+++ examples/multiple.py
@@ -1,10 +1,11 @@
 from pathlib import Path
+from typing import Optional
 
 from europarser import pipeline
 from europarser.models import FileToTransform, TransformerOutput, Params, Output
 
 
-def main(folder: Path, outputs: list[Output]) -> None:
+def main(folder: Path, outputs: list[Output], params: Optional[Params] = None) -> None:
     # parse all files
     files = []
     for file in folder.iterdir():
@@ -14,7 +15,7 @@ def main(folder: Path, outputs: list[Output]) -> None:
     print(f"Processing {len(files)} files...")
 
     # process result
-    results: list[TransformerOutput] = pipeline(files, outputs, Params())
+    results: list[TransformerOutput] = pipeline(files, outputs, params=params)
 
     # write results
     folder = folder / 'results'
@@ -23,7 +24,7 @@ def main(folder: Path, outputs: list[Output]) -> None:
         print(result.filename)
         if isinstance(result.data, str):
             with open(folder / f'{result.filename}', 'w', encoding='utf-8') as f:
-                    f.write(result.data)
+                f.write(result.data)
         else:
             with open(folder / f'{result.filename}', 'wb') as f:
                 f.write(result.data)
@@ -34,4 +35,10 @@ if __name__ == '__main__':
     folder = Path('/home/marceau/Nextcloud/eurocollectes/15-18')
     # outputs = ["json", "txm", "iramuteq", "csv", "stats", "processed_stats", "plots", "markdown"]
     outputs = ["json", "stats", "processed_stats", "plots"]
-    main(folder, outputs)
+    params = Params(
+        minimal_support_kw=5,
+        minimal_support_authors=2,
+        minimal_support_journals=8,
+        minimal_support_dates=3,
+    )
+    main(folder, outputs, params=params)
