e61d3fc7ef43acd33b3633715d7012c9f253c9dd
Marceau-h <git@marceau-h.fr>
2024-04-05 09:36:56 +0200

    Smol bash changes
    More alike to the other ones

--- start_europarser.sh
+++ start_europarser.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -a
-source .env_europarser > /dev/null 2>&1
+set -efaou pipefail
+
+source .env_txm4txm > /dev/null 2>&1
+
+set +a
 
 if [ "$EUROPARSER_OUTPUT" ]
 then
@@ -19,29 +22,51 @@ else
     fi
 fi
 
-set +a
+cd "${FOLDER:=`pwd`/}"
 
-cd "${FOLDER:=${pwd}}" || exit
+# Ensure pwd has worked
+if [ -z "${FOLDER%/*}" ]
+then
+    echo "The pwd command failed, leading to cd to $FOLDER"
+    exit 1
+fi
 
 # git pull origin master --quiet || exit
 
+
 if [ ! -d "venv" ]
 then
-    python3.11 -m venv venv || exit
-    pip install . || exit
+    python3.11 -m venv venv
+    source venv/bin/activate
+    pip install -r requirements.txt
 fi
-source "$FOLDER/venv/bin/activate" || exit
+
+source "$FOLDER""venv/bin/activate"
+
+# Ensure that we have everything we need
+for package in `cat requirements.txt`
+do
+    if ! pip show $package > /dev/null
+    then
+        echo "Missing $package, trying to install it..."
+        pip install $package
+    fi
+done
 
 COMMAND="source $FOLDER/venv/bin/activate; python -m uvicorn src.europarser.api.api:app --host ${EUROPARSER_HOST:-'0.0.0.0'} --port ${EUROPARSER_PORT:-'8000'} --root-path ${ROOT_PATH:-'/'} --workers 8 --timeout-keep-alive 1000 --log-config log.conf"
+
 printf "Starting europarser with command:\n"
 echo "$COMMAND"
 
+set +ue
 IS_RUNNING=$(ps -aux | grep uvicorn | grep europarser_api)
 if [ -z "$IS_RUNNING" ]
 then
+    set -ue
     echo "europarser service currently not running, starting gunicorn..."
     screen -S EuropressParser -dm bash -c "$COMMAND"
 else
+    set -ue
     echo "europarser already running, restarting..."
     screen -S EuropressParser -X quit
     screen -S EuropressParser -dm bash -c "$COMMAND"
