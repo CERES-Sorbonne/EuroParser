0bd4dcf03665144686444cee77e5055a77119c1e
Orion Alié <someonefefe@gmail.com>
2024-06-26 11:45:20 +0200

    Fix journal clean error in iramuteq

--- src/europarser/transformers/to_iramuteq.py
+++ src/europarser/transformers/to_iramuteq.py
@@ -13,12 +13,14 @@ class IramuteqTransformer(Transformer):
         self.output_type = "txt"
         self.output = TransformerOutput(data=None, output=self.output_type,
                                         filename=f'{self.name}_output.{self.output_type}')
-
+    def _to_camel(self, string):
+        return "".join(x.capitalize() for x in string.lower().split("_"))
+        
     def transform(self, pivot_list: List[Pivot]) -> TransformerOutput:
         with io.StringIO() as f:
             for pivot in pivot_list:
                 dic = pivot.model_dump(exclude=self.banned_keys)
-                f.write(f"""**** {' '.join([f"*{k}_{self._format_value(str(v))}" for k, v in dic.items()])}\n""")
+                f.write(f"""**** {' '.join([f"*{self._to_camel(k)}_{self._format_value(str(v))}" for k, v in dic.items()])}\n""")
                 f.write(pivot.texte)
                 f.write('\n\n')
             self.output.data = f.getvalue()
