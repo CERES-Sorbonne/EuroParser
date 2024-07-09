ca37f32f24038667b8cde317e313aba3ab1ab651
Marceau-h <git@marceau-h.fr>
2024-07-09 17:43:14 +0200

    WIP: The request

--- src/europarser/api/static/new_wui.js
+++ src/europarser/api/static/new_wui.js
@@ -1,6 +1,3 @@
-// import * as Popper from 'https://unpkg.com/@popperjs/core@2/dist/umd/popper.js';
-import 'https://unpkg.com/@popperjs/core@2/dist/umd/popper.js';
-
 document.getElementsByClassName('myInput').forEach(function (item) {
     item.style.margin = 'auto 1vh 1vh' + document.getElementById('myDropzone').offsetHeight + 'px' + ' 50vh';
 });
@@ -13,19 +10,64 @@ function saveBlob(blob, fileName) {
 }
 
 function createUrl(files, dataBlock) {
-    let url = "{{host + '/upload?'}}"
-        + "filter_keywords=" + document.getElementById('filter_keywords').checked.toString()
-        + "&minimal_support=" + (document.getElementById('minimal_support').value || 1)
-        + "&minimal_support_kw=" + (document.getElementById('minimal_support_kw').value || 1)
-        + "&minimal_support_journals=" + (document.getElementById('minimal_support_journals').value || 1)
-        + "&minimal_support_authors=" + (document.getElementById('minimal_support_authors').value || 1)
-        + "&minimal_support_dates=" + (document.getElementById('minimal_support_dates').value || 1)
+    // let url = "{{host + '/upload?'}}"
+    let url = "/upload?"
+        // + "filter_keywords=" + document.getElementById('filter_keywords').checked.toString()
+        // + "&minimal_support=" + (document.getElementById('minimal_support').value || 1)
+        // + "&minimal_support_kw=" + (document.getElementById('minimal_support_kw').value || 1)
+        // + "&minimal_support_journals=" + (document.getElementById('minimal_support_journals').value || 1)
+        // + "&minimal_support_authors=" + (document.getElementById('minimal_support_authors').value || 1)
+        // + "&minimal_support_dates=" + (document.getElementById('minimal_support_dates').value || 1)
 
     console.log(url)
     return (url)
 }
 
-Dropzone.options.myDropzone = {
+function submitForm() {
+    //send all the form data along with the files:
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+
+    let checkboxes = document.getElementsByTagName('input');
+    checkboxes = Array.from(checkboxes);
+
+    for (let checkbox of checkboxes) {
+        if (checkbox.type === 'checkbox' && checkbox.checked) {
+            formData.append("output", checkbox.id.toString().toLowerCase());
+        }
+    }
+
+    // TODO add the rest of the form data (files)
+    1/0
+
+    console.log(formData.get("output"))
+    xhr.open("POST", createUrl());
+
+    let len_ = checkboxes.length
+    for (let i = 0; i < len_; i++) {
+        checkboxes[i].disabled = true;
+        checkboxes[i].cursor = "not-allowed";
+        checkboxes[i].style.opacity = "0.5";
+    }
+    xhr.responseType = 'blob';
+    xhr.onload = function (e) {
+        if (e.currentTarget.status > 300) {
+            document.getElementById('loader').style.display = "none";
+            document.getElementById('error').innerHTML = e.currentTarget.statusText;
+            document.getElementById('error').style.display = "block";
+        }
+        var blob = e.currentTarget.response;
+        var contentDispo = e.currentTarget.getResponseHeader('Content-Disposition');
+        // https://stackoverflow.com/a/23054920/
+        var fileName = contentDispo.match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1];
+        document.getElementById('download').style.display = "block";
+        document.getElementById('loader').style.display = "none";
+        saveBlob(blob, fileName);
+    }
+    xhr.send(formData);
+}
+
+let data = Dropzone.options.myDropzone = {
     paramName: 'files',
     url: createUrl,
     autoProcessQueue: false,
@@ -33,59 +75,6 @@ Dropzone.options.myDropzone = {
     parallelUploads: 100,
     maxFiles: 100,
     acceptedFiles: '.html',
-    // addRemoveLinks: true,
-    // headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-
-    init: function () {
-        let dzClosure = this; // Makes sure that 'this' is understood inside the functions below.
-
-        // for Dropzone to process the queue (instead of default form behavior):
-        document.getElementById("submit-all").addEventListener("click", function (e) {
-            // Make sure that the form isn't actually being sent.
-            e.preventDefault();
-            e.stopPropagation();
-            dzClosure.processQueue();
-
-        })
-
-        //send all the form data along with the files:
-        this.on("sendingmultiple", function (data, xhr, formData) {
-            if (document.getElementById('iramuteq').checked) {
-                formData.append("output", "iramuteq");
-            }
-            if (document.getElementById('txm').checked) {
-                formData.append("output", "txm");
-            }
-            if (document.getElementById('json').checked) {
-                formData.append("output", "json");
-            }
-            if (document.getElementById('csv').checked) {
-                formData.append("output", "csv");
-            }
-            if (document.getElementById('excel').checked) {
-                formData.append("output", "excel");
-            }
-            if (document.getElementById('stats').checked) {
-                formData.append("output", "stats");
-            }
-            var checked;
-            if (document.getElementById('processed_stats').checked) {
-                formData.append("output", "processed_stats");
-                checked = true;
-            }
-            if (document.getElementById('plots').checked) {
-                formData.append("output", "plots");
-            }
-            if (document.getElementById('markdown').checked) {
-                formData.append("output", "markdown");
-            }
-
-            for (let file of data) {
-                if (file.size === 0) { // Directory
-                    // pass
-                } else { // File
-                    formData.append("files", file);
-                }
-            }
+}
 
 
