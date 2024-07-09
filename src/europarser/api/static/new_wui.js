6446702ad54d309c4db2a76e4e45ebc7ee673da1
Marceau-h <git@marceau-h.fr>
2024-07-09 17:19:10 +0200

    cleanup

--- src/europarser/api/static/new_wui.js
+++ src/europarser/api/static/new_wui.js
@@ -120,65 +120,8 @@ Dropzone.options.myDropzone = {
     }
 }
 
-
-// const button = document.querySelector('#button');
-// const tooltip = document.querySelector('#tooltip');
-//
-// const popperInstance = window.Popper.createPopper(button, tooltip, {
-//     modifiers: [
-//         {
-//             name: 'offset',
-//             options: {
-//                 offset: [0, 8],
-//             },
-//         },
-//     ],
-// });
-//
-// function show() {
-//     // Make the tooltip visible
-//     tooltip.setAttribute('data-show', '');
-//
-//     // Enable the event listeners
-//     popperInstance.setOptions((options) => ({
-//         ...options,
-//         modifiers: [
-//             ...options.modifiers,
-//             {name: 'eventListeners', enabled: true},
-//         ],
-//     }));
-//
-//     // Update its position
-//     popperInstance.update();
-// }
-//
-// function hide() {
-//     // Hide the tooltip
-//     tooltip.removeAttribute('data-show');
-//
-//     // Disable the event listeners
-//     popperInstance.setOptions((options) => ({
-//         ...options,
-//         modifiers: [
-//             ...options.modifiers,
-//             {name: 'eventListeners', enabled: false},
-//         ],
-//     }));
-// }
-//
-// const showEvents = ['mouseenter', 'focus'];
-// const hideEvents = ['mouseleave', 'blur'];
-//
-// showEvents.forEach((event) => {
-//     button.addEventListener(event, show);
-// });
-//
-// hideEvents.forEach((event) => {
-//     button.addEventListener(event, hide);
-// });
-
+// Popper.js
 const tooltip_triggers = document.querySelectorAll(".tooltip-trigger");
-// const tooltip = document.querySelectorAll(".tooltip");
 
 tooltip_triggers.forEach((trigger) => {
         const parent = trigger.parentElement;
